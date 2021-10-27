6947a9627cd84a46ac9c781d43e1d168c3cc36ae
Rohit Dwivedula <rohitdwivedula@gmail.com>
2021-10-28 00:54:16 +0530

    simplify Go sign() and verify() args to make it easier to send stuff from C

--- messagesign.go
+++ messagesign.go
@@ -11,11 +11,13 @@ import (
 	crand "crypto/rand"
 	"encoding/hex"
 	"fmt"
+	"strings"
 
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcutil"
 )
+import "strconv"
 
 // generateKeyPair generates and stores an ECDSA keypair to a file.
 //export generateKeyPair
@@ -38,7 +40,24 @@ func generateKeyPair() map[string]string {
 }
 
 //export sign
-func sign(keyPair map[string]string, keyRing map[string]string, message string) string {
+func sign(keyPair_t string, keyRing_t string, message string) string {
+	keyPair := make(map[string]string)
+	keyRing := make(map[string]string)
+
+	split1 := strings.Split(keyPair_t, " ")
+	split2 := strings.Split(keyRing_t, " ")
+
+	keyPair["address"] = split1[0]
+	keyPair["privkey"] = split1[1]
+	keyPair["pubkey"] = split1[2]
+
+	for i := 0; i < len(split2); i++ {
+		keyRing[strconv.Itoa(i)] = split2[i]
+	}
+
+	fmt.Printf("[SIGN] Keypair: %v\n\n", keyPair)
+	fmt.Printf("[SIGN] Keyring: %v\n\n", keyRing)
+
 	kp, err := ParseKeyPair(keyPair)
 	if err != nil {
 		return ""
@@ -56,7 +75,13 @@ func sign(keyPair map[string]string, keyRing map[string]string, message string) 
 }
 
 //export verify
-func verify(keyRing map[string]string, message string, signature string) bool {
+func verify(keyRing_t string, message string, signature string) bool {
+	keyRing := make(map[string]string)
+	split := strings.Split(keyRing_t, " ")
+	for i := 0; i < len(split); i++ {
+		keyRing[strconv.Itoa(i)] = split[i]
+	}
+
 	kr, err := ParseKeyRing(keyRing, nil)
 	if err != nil {
 		fmt.Printf("[ERROR GoLang] Could not parse keyring: %v\n", err)
@@ -72,5 +97,10 @@ func verify(keyRing map[string]string, message string, signature string) bool {
 }
 
 func main() {
-
+	s := sign(
+		"1NBN9d4pZutCykB3Why5f3V7hG27EbcqKb 4fb0b355ad56c1d19ebb30591a036dfb6a2c20d9836b22c23dc521ea53e08cd4 02dcdb96d05d6cd36ce7014a69ebce8b48f8d7de46ce3bfa99482af65284697e13",
+		"024627032575180c2773b3eedd3a163dc2f3c6c84f9d0a1fc561a9578a15e6d0e3 02b266b2c32ba5fc8d203c8f3e65e50480dfc10404ed089bad5f9ac5a45ffa4251 031ea759e3401463b82e2132535393076dde89bf2af7fc550f0793126669ffb5cd",
+		"ROHIT",
+	)
+	fmt.Printf("SINATURE: %v\n", s)
 }
